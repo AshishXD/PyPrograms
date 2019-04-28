30c493fe6051fc76b3798507bcbb560f6ef4d4cb
Ashish Kumar <28337472+AshishXD@users.noreply.github.com>
2019-04-29 02:30:11 +0530

    PyPrograms

---
+++ Decorator.py
@@ -0,0 +1,16 @@
+def div(a, b):
+    print(a/b)
+
+
+def smart_div(func):
+
+    def inner(a, b):
+        if a < b:
+            a, b = b, a
+        return func(a, b)
+
+    return inner
+
+
+div_new = smart_div(div)
+div_new(2, 4)
